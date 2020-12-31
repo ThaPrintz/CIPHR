cf8d109b4f4293c1ead71abd3a2359435985ef59
Skyler Nelson <45441127+ThaPrintz@users.noreply.github.com>
2020-12-31 01:37:56 -0700

    began server functionality, and added routines to the client for server testing

--- CIPHR cl/MainWindow.xaml.cs
+++ CIPHR cl/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Remoting;
 
 namespace CIPHR
 {
@@ -23,6 +27,25 @@ namespace CIPHR
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AuthLogin(object sender, RoutedEventArgs e)
+        {
+            System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+
+            try
+            {
+                clientSocket.Connect("127.0.0.1", 70);
+
+                NetworkStream serverStream = clientSocket.GetStream();
+
+                byte[] msg = Encoding.ASCII.GetBytes("[LOGIN_AUTH]username:password");
+
+                serverStream.Write(msg, 0, msg.Length);
+                serverStream.Flush();
+            } catch ( Exception ex) {
+
+            }
         }
     }
 }
---
+++ CIPHR sv/rsc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define WINDOWS_LEAN_AND_MEAN
+
+#ifndef CIPHR_H
+#define CIPHR_H
+
+#include <webxlib.h>
+
+#endif //CIPHR_H
