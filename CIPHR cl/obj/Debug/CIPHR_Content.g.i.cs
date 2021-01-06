043d70be6d20dc25963e774ae18cb677b0f07c0e
Skyler Nelson <45441127+ThaPrintz@users.noreply.github.com>
2021-01-05 23:45:20 -0700

    began working on the main client, so far just early stages of design and no functionality yet

--- CIPHR cl/obj/Debug/CIPHR_Content.g.i.cs
+++ CIPHR cl/obj/Debug/CIPHR_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("clogoo.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("ww.png")]
 
 
