87d36555c6811fec0ef1dbb23279267c1c3fdd33
Skyler Nelson <45441127+ThaPrintz@users.noreply.github.com>
2021-01-12 03:18:44 -0700

    began skeleton of 'sv_netcmd', this will be my own network protocol i use for all data transports in CIPHR, login/register/server chat/voice etc

---
+++ CIPHR sv/sv_netcmd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIPHR_server
+{
+    class sv_netcmd
+    {
+        public static int AUTH(string u, string p)
+        {
+            return 0;
+        }
+
+        public static int REG(string u, string p)
+        {
+            return 0;
+        }
+
+
+    }
+}
